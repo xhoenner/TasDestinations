d2d880af02155de59cfe82e3637fc20808644679
Xavier Hoenner <xavier.hoenner@csiro.au>
2021-12-16 21:44:36 +1100

    Add in all bushwalks and improve popups

--- TasmaniaDestinations.R
+++ TasmaniaDestinations.R
@@ -13,7 +13,13 @@ map <- resources %>%
     addLayersControl(baseGroups = c("OpenStreetMap", "Esri.WorldImagery")) %>%
     setView(lng = 146.75, lat = -42.1, zoom = 8) %>%
   	addMiniMap() %>% addMeasure(primaryLengthUnit = 'kilometers') %>%
-  	addCircleMarkers(~LON, ~LAT, color = ~cof(Type), fill = ~cof(Type), label = resources$Name, clusterOptions = markerClusterOptions(), popup = paste0(resources$Name, ': ', resources$Description)) %>%
+  	addCircleMarkers(~LON, ~LAT, color = ~cof(Type), fill = ~cof(Type), label = resources$Name, clusterOptions = markerClusterOptions(), 
+  		popup = paste0('<b>', resources$Name, ': </b>', "<br>", 
+  						resources$Description, "<br>", 
+  						paste0("<img src = ", resources$Photo, " width='300' height='200'>"), "<br>", 
+  						"<a href='", resources$Resources, "' target='_blank'>"
+		                 , "More information</a>"),
+    labelOptions = labelOptions(style = list("font-weight" = "normal", padding = "3px 8px", textsize = "15px", direction = "auto" ))) %>%
   	addLegend("bottomright", colors= c("#ffa500", "#13ED3F"), labels=c("Bushwalk", "Restaurants/Cafes"), title="Things to do") 
   	
 # bscols(filter_select("Location", "Location", resources_sd, ~Location))
