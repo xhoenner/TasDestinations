438484b8a76d2979e51fae08be9938ed19d6a531
Xavier Hoenner <xavier.hoenner@csiro.au>
2021-12-17 21:31:26 +1100

    OpenWeatherMapTiles, ResetView button, Removed stroke on markers, increased popup font size

--- TasmaniaDestinations.R
+++ TasmaniaDestinations.R
@@ -1,7 +1,7 @@
 rm(list=ls())
 
-library(leaflet); library(mapview); library(readxl); #library(crosstalk)
-
+library(leaflet); library(mapview); library(readxl); library(leaflet.extras); library(leaflet.extras2); #library(crosstalk)
+Sys.setenv("OPENWEATHERMAP" = 'e8f90cc547708add27f4cfcf9fc851f6')
 resources <- read_excel('~/Downloads/WIP/TasDestinations/TasmaniaDestinations.xlsx')
 # resources_sd <- SharedData$new(resources)
 cof <- colorFactor(c("#ffa500", "#13ED3F"), domain=c("Bushwalk", "Restaurants/Cafes"))
@@ -10,16 +10,22 @@ map <- resources %>%
 	leaflet(options = leafletOptions(preferCanvas = TRUE)) %>%
     addProviderTiles(providers$OpenStreetMap, group = "OpenStreetMap", options = providerTileOptions(updateWhenZooming = FALSE, updateWhenIdle = FALSE)) %>% 
     addProviderTiles(providers$Esri.WorldImagery, group = "Esri.WorldImagery") %>% 
-    addLayersControl(baseGroups = c("OpenStreetMap", "Esri.WorldImagery")) %>%
+    addProviderTiles(providers$OpenWeatherMap.Wind, group = 'Wind speed', options = providerTileOptions(apiKey="e8f90cc547708add27f4cfcf9fc851f6")) %>% ## Wind
+    addProviderTiles(providers$OpenWeatherMap.RainClassic, group = 'Rain', options = providerTileOptions(apiKey="e8f90cc547708add27f4cfcf9fc851f6")) %>% ## Rain
+    addProviderTiles(providers$OpenWeatherMap.Temperature, group = 'Temperature', options = providerTileOptions(apiKey="e8f90cc547708add27f4cfcf9fc851f6")) %>% ## Temperature
+    addLayersControl(baseGroups = c("OpenStreetMap", "Esri.WorldImagery"), overlayGroups = c('Wind speed', 'Rain', 'Temperature')) %>%
+    hideGroup(c('Wind speed', 'Rain', 'Temperature')) %>%
     setView(lng = 146.75, lat = -42.1, zoom = 8) %>%
-  	addMiniMap() %>% addMeasure(primaryLengthUnit = 'kilometers') %>%
-  	addCircleMarkers(~LON, ~LAT, color = ~cof(Type), fill = ~cof(Type), label = resources$Name, clusterOptions = markerClusterOptions(), 
+    addResetMapButton() %>%
+  	addMiniMap() %>% 
+  	addMeasure(primaryLengthUnit = 'kilometers') %>%
+  	addCircleMarkers(~LON, ~LAT, color = ~cof(Type), fill = ~cof(Type), label = resources$Name, stroke = FALSE, fillOpacity = 0.5, clusterOptions = markerClusterOptions(), 
   		popup = paste0('<b>', resources$Name, ': </b>', "<br>", 
   						resources$Description, "<br>", 
   						paste0("<img src = ", resources$Photo, " width='300' height='200'>"), "<br>", 
   						"<a href='", resources$Resources, "' target='_blank'>"
-		                 , "More information</a>"),
-    labelOptions = labelOptions(style = list("font-weight" = "normal", padding = "3px 8px", textsize = "15px", direction = "auto" ))) %>%
+		                 , "More info</a>"),
+    labelOptions = labelOptions(style = list("font-weight" = "normal", padding = "3px 8px", textsize = "20px", direction = "auto"))) %>%
   	addLegend("bottomright", colors= c("#ffa500", "#13ED3F"), labels=c("Bushwalk", "Restaurants/Cafes"), title="Things to do") 
   	
 # bscols(filter_select("Location", "Location", resources_sd, ~Location))
